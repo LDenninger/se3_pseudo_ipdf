Fixed PLS for tabletop dataset: Added conversion opencv/opengl, fixed right scaling for depth imgs

pose_labeling_scheme/pose_labeling_scheme.py
@@ -16,9 +16,18 @@ def pose_labeling_scheme(pts_canonical, seg_data, depth_data, diameter, intrinsi
     seg_data = seg_data.squeeze()
     depth_data = depth_data.squeeze()
     intrinsic = intrinsic.squeeze()
+    if config["dataset"]=="tabletop":
+        #Mask depth image for tabletop dataset
+        object_pixel = seg_data==config["obj_id"]
+        object_pixel = torch.nonzero(object_pixel)
+        mask = torch.zeros_like(depth_data)
+        mask[object_pixel[:,0], object_pixel[:,1]] = 1
+        depth_data *= mask
+
     # Extract and process point clouds
     # Scale everything to cm for optimization
-    pts_observed = generate_pointcloud_from_rgbd(config["dataset"], seg_data, depth_data, intrinsic)
+    pts_observed = generate_pointcloud_from_rgbd(config["dataset"], seg_data, depth_data, intrinsic,
+                                                    obj_id = config["obj_id"] if config["dataset"]=="tabletop" else 1)
     pts_canonical = pts_canonical*100
     diameter = diameter*100
     # Construct open3d point clouds. Additionally downsampling, fpfh features, and normals approximation
@@ -109,6 +118,7 @@ def pose_labeling_scheme(pts_canonical, seg_data, depth_data, diameter, intrinsi
 
         # Scale translation back to meters
         pseudo_transformation[:,:3,-1] /= 100
+        # SVD for numerical stability
         U, S, V = torch.linalg.svd(pseudo_transformation[:,:3,:3])
         pseudo_transformation[:,:3,:3] = torch.bmm(U, V)
         if not config['verbose']:
@@ -157,11 +167,12 @@ def pose_labeling_scheme(pts_canonical, seg_data, depth_data, diameter, intrinsi
                 #print("\nConverge: ", converge)
                 print("\n_________________________________________________\n")
             ipdb.set_trace()
-
                 #viz.draw_registration_result(final_pcd, target_down_local, np.identity(4), lookat=pseudo_transformation[:3,-1].cpu().numpy())
         #ipdb.set_trace()
     if pseudo_ground_truth_set == []:
         return None
+    if config["dataset"]=="tabletop":
+        pseudo_ground_truth_set = convert_transformation_opencv_opengl(pseudo_ground_truth_set)
     return torch.stack(pseudo_ground_truth_set)
 
 

pose_labeling_scheme/registration/convergence/rnc_convergence.py
@@ -24,9 +24,12 @@ def imshow(input: torch.Tensor, name: str):
 
 def render_depth_image(obj_model, transformation, intrinsic, config):
     # Render the depth using the cad model
-    K = intrinsic # (fx, fy, cx, cy)
     scene = sl.Scene(config["resolution"])
-    scene.set_camera_intrinsics(*K)
+    if config["dataset"]=="tabletop":
+        scene.set_camera_hfov( (90 / 180) * np.pi )
+    else:
+        K = intrinsic # (fx, fy, cx, cy)
+        scene.set_camera_intrinsics(*K)
     scene.ambient_light = torch.tensor([0.9, 0.9, 0.9])
     obj = sl.Object(obj_model)
     # import ipdb; ipdb.set_trace() 
@@ -63,6 +66,7 @@ def matcher(img_1, img_2, threshold=None, verbose=False):
     B = img_1.shape[0]
     d_max = torch.zeros(B)
     d_avg = torch.zeros(B)
+
 
     if verbose:
         diff_img = torch.zeros_like(img_1)
@@ -102,15 +106,17 @@ def check_convergence_batchwise(obj_model, transformation_set, depth_original, i
     # Render the depth from the final pseudo transformation
     depth_rendered = []
     threshold = config["threshold"]
+
     for (i, transformation) in enumerate(transformation_set):
         depth_rendered.append(render_depth_image(obj_model, transformation, intrinsic, config))
     depth_rendered = torch.stack(depth_rendered).to(DEVICE)
-    depth_original = depth_original.unsqueeze(0).to(DEVICE)
-    
-    # Scale to cm
-    depth_rendered = depth_rendered * 100 
-    depth_original = depth_original / 10
+    depth_rendered = depth_rendered * 100 # scale to cm
 
+    depth_original = depth_original.unsqueeze(0).to(DEVICE)
+    if config["dataset"]=="tabletop":
+        depth_original = -1 * depth_original * 100
+    else:
+        depth_original = depth_original / 10
     # Compute edge image of the final result using Canny edge detector
     edge_rendered = process_tensor(depth_rendered)
     edge_rendered = binarize(edge_rendered)   # binarize to get sharp edges

run_pose_labeling_scheme.py
@@ -2,11 +2,14 @@ import torch
 import argparse
 from tqdm import tqdm
 import os
+import ipdb
 
 import data
 import utils
 import config
 from pose_labeling_scheme import pose_labeling_scheme
+from pose_labeling_scheme.utils import convert_points_opencv_opengl
+import utils.visualizations as visualizations
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -37,8 +40,11 @@ if __name__=="__main__":
 
     # Load the object model
     if args.dataset=="tabletop":
-        object_model, diameter = data.load_tless_object_model(args.obj_id, pointcloud_only=True)
+        object_model, diameter = data.load_ycbv_object_model(args.obj_id, pointcloud_only=True)
+        object_model = convert_points_opencv_opengl(object_model) # Convert opeGL to openCV
         object_model_sl = data.load_sl_cad_model(args.dataset, args.obj_id)
+        # Convert canonical point clouds to fit the OpenCV convention
+
     elif args.dataset=="tless":
         object_model, diameter = data.load_tless_object_model(args.obj_id, pointcloud_only=True)
         object_model_sl = data.load_sl_cad_model(args.dataset, args.obj_id)
@@ -53,10 +59,11 @@ if __name__=="__main__":
             break
         if not input["loaded"]:
             continue
+
         
-        seg_data = input["seg_image"].to(DEVICE)
-        depth_data = input["depth_image"].to(DEVICE)
-        intrinsic = input["intrinsic"].to(DEVICE)
+        seg_data = input["seg_image"].to(DEVICE).squeeze()
+        depth_data = input["depth_image"].to(DEVICE).squeeze()
+        intrinsic = input["intrinsic"].to(DEVICE).squeeze()
         
         pseudo_ground_truth = pose_labeling_scheme(pts_canonical=object_model,
                                                     seg_data=seg_data,
@@ -80,10 +87,21 @@ if __name__=="__main__":
 
             elif args.dataset=="tabletop":
                 save_dir = os.path.join(data.id_to_path[args.obj_id], str(i).zfill(6), "pseudo_gt.pth")
-                if os.path.exists(save_dir):
+                if False and os.path.exists(save_dir):
                     pgt_exist = torch.load(save_dir)
                     pseudo_ground_truth = torch.cat((pgt_exist, pseudo_ground_truth))
                 torch.save(pseudo_ground_truth, save_dir)
+        else:
+            visualizations.visualize_so3_rotations(
+                rotations=pseudo_ground_truth,
+                dataset=config["dataset"],
+                obj_id=config["obj_id"],
+                rotations_gt=input["ground_truth"],
+                display_gt_set=True,
+                save_path="output/pose_labeling_scheme/pgt_final_result.png"
+            )
+            ipdb.set_trace()
+        
         
             
     print("_"*20)